ffe43786155576dbbabdde7fb4b773fa931af0d1
Matthew Heering <matthew.c.heering01@gmail.com>
2021-04-24 13:39:11 -0500

    adding .env an procfile

--- server/index.js
+++ server/index.js
@@ -2,25 +2,30 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import { aggregateSubjects } from './controllers/aggregateCards.js';
 
 import createCard from './routes/createCard.js';
 import userRoutes from './routes/users.js';
+import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.get('/', (req, res) => {
+      res.send("Welcome to Flash Learn API")
+})
+
 app.use('/cards', createCard)
 app.use('/user', userRoutes)
-//MongoDB connection
-const CONNECTION_URL = 'mongodb+srv://flashcard:y5WxSmGa6gUThmW0@cluster0.lly57.mongodb.net/AllFlashCards?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
       .then(() => {
             app.listen(PORT, () => {
                   console.log(`Server running on port: ${PORT}`)
